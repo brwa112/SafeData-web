1f30a5efd8c92fbfb2455c2eecfb2bb5d08cfd7b
ramyar-test <mameamrm@gmail.com>
2025-12-07 20:10:23 +0300

    inp

--- app/Http/Controllers/System/Settings/Settings/SyncTranslationController.php
+++ app/Http/Controllers/System/Settings/Settings/SyncTranslationController.php
@@ -10,7 +10,13 @@ class SyncTranslationController extends Controller
 {
     public function syncTranslations(Request $request)
     {
+        // First, import translations from PHP files to database
+        Artisan::call('db:seed', ['--class' => 'TranslationsSeeder']);
+        
+        // Then, cache translations from database to JSON files
         Artisan::call('translations:cache');
+        
+        return redirect()->back()->with('success', __('common.updated'));
     }
 }
 
